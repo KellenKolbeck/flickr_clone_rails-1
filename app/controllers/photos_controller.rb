ce3ed1d85aa664bfa348ef8c20afe4bda6a019e5
Bunker & Josue Valverde <student@epicodus.com>
2015-10-21 11:20:11 -0700

    add Delete function for images

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -22,6 +22,16 @@ class PhotosController < ApplicationController
     end
   end
 
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def destroy
+    photo = Photo.find(params[:id])
+    photo.destroy
+    redirect_to photos_path
+  end
+
 private
 
   def photo_params
