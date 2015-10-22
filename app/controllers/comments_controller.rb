7bca2d9fe3b07ced865050db146159936dc7cc0c
B & J <student@epicodus.com>
2015-10-22 11:48:43 -0700

    add crud functionality to comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,28 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+    @photo = Photo.find(params[:photo_id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @photo = Photo.find(params[:photo_id])
+    if @comment.update(comment_params)
+      redirect_to photo_path(@photo)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    photo = Photo.find(params[:photo_id])
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to photo_path(photo)
+  end
+
 private
 
   def comment_params
